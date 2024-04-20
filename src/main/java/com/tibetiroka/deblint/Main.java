0c0f3725af30c1c6ef0087c2c078fd76e3549a0b
tibetiroka <tibetiroka@gmail.com>
2024-04-20 15:49:11 +0200

    Prepare v1.1.1 release

--- src/main/java/com/tibetiroka/deblint/Main.java
+++ src/main/java/com/tibetiroka/deblint/Main.java
@@ -20,7 +20,7 @@ public class Main {
 	/**
 	 * The current version of the project
 	 */
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "1.1.1";
 	/**
 	 * Stores whether the app is running inside a native image.
 	 */
